10c0c83acb7db62b2645bd7dea69f441b4803109
palma001 <palmaluis1997@gmail.com>
2020-02-18 18:20:02 -0400

    table users

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
+Route::get('/users', 'UsersController@index')->name('users');
